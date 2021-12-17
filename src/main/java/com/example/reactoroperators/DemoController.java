6dccdb3740e84c65c1b81fe1df965a98c64b9e55
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2021-12-16 21:08:05 -0500

    parallism

--- src/main/java/com/example/reactoroperators/DemoController.java
+++ src/main/java/com/example/reactoroperators/DemoController.java
@@ -1,19 +1,14 @@
 package com.example.reactoroperators;
 
-import java.util.Locale;
-import java.util.concurrent.Callable;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.CorePublisher;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @Slf4j
@@ -21,43 +16,55 @@ public class DemoController {
 
   @GetMapping("api/operators")
   public Publisher<?> operators() {
-    return Mono.just(1) // Mono<Integer>
-      .flatMap(integer -> convertirALetras(integer)) // Mono<IntegerToLettersWrapper>
-      .map(x -> {
-        Integer integer = x.getInteger();
-        String letters = x.getLetters();
-        log.info("integer: {}", integer);
-        log.info("letters: {}", letters);
-        return x.getLetters();
-      }); // Mono<String>
+    Mono<DataWS> llamarAWs01 = Mono.fromCallable(() -> getData("RESUL"));//.subscribeOn(Schedulers.parallel());
+    Mono<DataWS> llamarAWs02 = Mono.fromCallable(() -> getData("TADO"));//.subscribeOn(Schedulers.boundedElastic());
+
+    return Mono
+      .zip(llamarAWs01, llamarAWs02, DemoController::concatTwoDatas)
+      .subscribeOn(Schedulers.boundedElastic())
+      .publishOn(Schedulers.newParallel("mi-hilito"))
+      .map(DemoController::convertToFinalResult);
   }
 
-  Mono<IntegerToLettersWrapper> convertirALetras(Integer i) {
-    return Mono.just(new IntegerToLettersWrapper("Uno", i));
+  private static Data02 concatTwoDatas(DataWS resultadoMono01, DataWS resultadoMono02) {
+    log.info("con el input inicial era: {}", resultadoMono01.getParameter() + resultadoMono02.getParameter());
+    var concatenation = resultadoMono01.getResult() + resultadoMono02.getResult();
+    return Data02.builder()
+      .parameter01(resultadoMono01.getParameter())
+      .parameter02(resultadoMono02.getParameter())
+      .concatenationResult(concatenation)
+      .build();
   }
 
-  @Data
-  @NoArgsConstructor
-  @AllArgsConstructor
-  static class IntegerToLettersWrapper {
-    private String letters;
-    private Integer integer;
+  private static String convertToFinalResult(Data02 data) {
+    log.info("convertToFinalResult");
+    return data.getParameter01() + "*" + data.getParameter02() + "-" + data.getConcatenationResult();
   }
 
-  Mono<String> libreriaDeMiEmpresa() {
-    return Mono.just(getDataFromWS());
+  private DataWS getData(String data) {
+    sleep(5_000);
+    log.info("getData({})", data);
+    return new DataWS(data, data.toLowerCase());
   }
 
-  // Aplicacion migrando -> Callable
-  Supplier<String> getData1() {
-    return () -> "X";
+  @SneakyThrows
+  private static void sleep(long time) {
+    Thread.sleep(time);
   }
 
-  String getDataFromWS() {
-    log.info("getDataFromWS");
-    return "Empleado activo"; // Llamo a un WS y trajo cierta data
+  @Builder
+  @Getter
+  static class DataWS {
+    private String parameter;
+    private String result;
   }
 
-
+  @Builder
+  @Getter
+  static class Data02 {
+    private String parameter01;
+    private String parameter02;
+    private String concatenationResult;
+  }
 
 }
