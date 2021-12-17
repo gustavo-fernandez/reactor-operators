51d942de4c181c53d4b3cf51a37506c43689d7aa
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2021-12-16 19:28:57 -0500

    initial commit

---
+++ src/main/java/com/example/reactoroperators/ReactorOperatorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactoroperators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorOperatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorOperatorsApplication.class, args);
+	}
+
+}
